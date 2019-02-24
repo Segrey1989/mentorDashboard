77c6e074d506f7cecdffe88b9f33dc1e896cfbfd
Siarhei Kvachonak <segrey1989@tut.by>
2019-02-24 10:17:38 +0000

    server.js

--- server/server.js
+++ server/server.js
@@ -1,50 +1,50 @@
 require('../googleApi/index');
 
 setTimeout(() => {
-    const makeJson = require('../src/mentorsInfo/index');
-    const mentors = Array.from(JSON.parse(makeJson.makeMentorsJson()));
+  const makeJson = require('../src/mentorsInfo/index');
+  const mentors = Array.from(JSON.parse(makeJson.makeMentorsJson()));
 
-    const express = require('express');
-    const app = express();
-    const port = 3000;
+  const express = require('express');
+  const app = express();
+  const port = 3000;
 
-    app.use(function(request, response, next) {
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        next();
+  app.use(function(request, response, next) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
+  app.get('/mentors', (request, response) => {
+    response.send(makeJson.makeMentorsJson());
+  });
+
+  app.get('/students', (request, response) => {
+    response.send(makeJson.makeStudentsJson());
+  });
+
+  app.get('/tasks', (request, response) => {
+    response.send(makeJson.makeTasksJson());
+  });
+
+  app.use('/:github', function(request, response) {
+    const mentorGithub = request.params.github;
+    const chosenMentor = mentors.find(mentor => {
+      if (mentor.github === `https://github.com/${mentorGithub}`) {
+        return mentor;
+      }
     });
+    if (chosenMentor) response.send(JSON.stringify(chosenMentor));
+    else response.send(false);
+  });
 
-    app.get('/mentors', (request, response) => {
-        response.send(makeJson.makeMentorsJson());
-    });
+  app.use((err, request, response, next) => {
+    console.log(err);
+    response.status(500).send('Something broke!');
+  });
 
-    app.get('/students', (request, response) => {
-        response.send(makeJson.makeStudentsJson());
-    });
-
-    app.get('/tasks', (request, response) => {
-        response.send(makeJson.makeTasksJson());
-    });
-
-    app.use('/:github', function(request, response) {
-        const mentorGithub = request.params.github;
-        const chosenMentor = mentors.find(mentor => {
-            if (mentor.github === `https://github.com/${mentorGithub}`) {
-                return mentor;
-            }
-        });
-        if (chosenMentor) response.send(JSON.stringify(chosenMentor));
-        else response.send(false);
-    });
-
-    app.use((err, request, response, next) => {
-        console.log(err);
-        response.status(500).send('Something broke!');
-    });
-
-    app.listen(port, err => {
-        if (err) {
-            return console.log('something bad happened', err);
-        }
-        console.log(`server is listening on ${port}`);
-    });
+  app.listen(port, err => {
+    if (err) {
+      return console.log('something bad happened', err);
+    }
+    console.log(`server is listening on ${port}`);
+  });
 }, 3000);
